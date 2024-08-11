068f2e779b236dedad2295bd86a3047635880c15
bus1hero <petru.cervac@gmail.com>
2024-08-11 14:51:17 +0300

    feat: add property tests for arrays of zeros and ones

--- CA2.Tests/UnitTest1.cs
+++ CA2.Tests/UnitTest1.cs
@@ -1,4 +1,7 @@
+using System.Numerics;
 using FluentAssertions;
+using FsCheck;
+using FsCheck.Xunit;
 
 namespace CA2.Tests;
 
@@ -30,21 +33,95 @@ public class UnitTest1
 
         number.Should().Be(expectedResult);
     }
+
+    [Property(Arbitrary = [typeof(BigArraysOfZerosAndOnes)])]
+    public Property ResultIsBiggerOrEqualToZero(
+        int[] numbers)
+    {
+        var generator = new Generator();
+
+        var number = generator.Generate(numbers);
+
+        return (0 <= number).ToProperty();
+    }
+
+    [Property(Arbitrary = [typeof(BigArraysOfZerosAndOnes)])]
+    public Property ResultIsSmallerOrEqualToTheCount(
+        int[] numbers)
+    {
+        var generator = new Generator();
+
+        var number = generator.Generate(numbers);
+
+        return (number < BigInteger.Pow(2, numbers.Length)).ToProperty();
+    }
+
+    [Property(Arbitrary = [typeof(BigArraysOfZeros)])]
+    public Property ArraysOfZerosHaveZero(int[] numbers)
+    {
+        var generator = new Generator();
+
+        var number = generator.Generate(numbers);
+
+        return (number == 0).ToProperty();
+    }
+
+    [Property(Arbitrary = [typeof(BigArraysOfOnes)])]
+    public Property ArraysOfOnesHaveMaxPower(int[] numbers)
+    {
+        var generator = new Generator();
+
+        var number = generator.Generate(numbers);
+
+        return (number == BigInteger.Pow(2, numbers.Length) - 1).ToProperty();
+    }
+}
+
+public static class BigArraysOfOnes
+{
+    public static Arbitrary<int[]> Generate()
+    {
+        return Gen.Elements(1).ArrayOf().ToArbitrary();
+    }
+}
+
+public static class BigArraysOfZeros
+{
+    public static Arbitrary<int[]> Generate()
+    {
+        return Gen.Elements(0).ArrayOf().ToArbitrary();
+    }
+}
+
+public static class BigArraysOfZerosAndOnes
+{
+    public static Arbitrary<int[]> Generate()
+    {
+        return Gen
+            .Elements(0, 1)
+            .ArrayOf()
+            .ToArbitrary();
+    }
 }
 
 public sealed class Generator
 {
-    public long Generate(int[] values)
+    public BigInteger Generate(int[] values)
     {
-        var result = 0;
-        
+        BigInteger result = 0;
+
         for (var i = 0; i < values.Length - 1; i++)
         {
             result *= 2;
             result += values[i] * 2;
         }
 
-        return result + values[^1];
+        if (values.Length != 0)
+        {
+            return result + values[^1];
+        }
+
+        return result;
     }
 }
 /*

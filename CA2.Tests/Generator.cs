91521f12f7e0eb6c1526ee1fc31e07972f03199b
bus1hero <petru.cervac@gmail.com>
2024-08-26 21:52:12 +0300

    feat: finally optimize the algorithm

--- CA2.Tests/Generator.cs
+++ CA2.Tests/Generator.cs
@@ -4,24 +4,24 @@ namespace CA2.Tests;
 
 public sealed class Generator
 {
-    public static BigInteger Generate(int[] values, int[] sizes)
+    public static BigInteger Generate(
+        int[] values,
+        int[] sizes)
     {
         if (values.Length == 0)
         {
             return 0;
         }
 
-        sizes = sizes.Reverse().ToArray();
-        values = values.Reverse().ToArray();
-        BigInteger result = values[0];
+        BigInteger result = values[^1];
         var power = BigInteger.One;
-        
-        for (var i = 1; i < sizes.Length; i++)
+
+        for (var i = sizes.Length - 2; 0 <= i; i--)
         {
-            power *= sizes[i - 1];
+            power *= sizes[i + 1];
             result += values[i] * power;
         }
-        
+
         return result;
     }
 }

62cc277cfbe868179d3959adbb71d6a3f3409cfa
bus1hero <petru.cervac@gmail.com>
2024-09-28 22:42:07 +0300

    feat: add new tests for CSV file generation

---
+++ CA2.Tests/CsvGenerationTests/CsvGeneratorToFileTests.cs
@@ -0,0 +1,109 @@
+﻿namespace CA2.Tests.CsvGenerationTests;
+
+using System.IO.Abstractions.TestingHelpers;
+
+using GeneratorLibrary;
+
+public class CsvGeneratorToFileTests
+{
+    [Theory, AutoData]
+    public async Task FileIsCreated(
+        string filename,
+        int rowsCount,
+        string[][] columns)
+    {
+        var fileSystem = new MockFileSystem();
+
+        var generator = new CsvGeneratorToFile(fileSystem);
+
+        await generator.Generate(
+            ".",
+            filename,
+            rowsCount,
+            columns);
+
+        fileSystem
+            .File
+            .Exists(filename)
+            .Should()
+            .BeTrue();
+    }
+
+    [Theory, AutoData]
+    public async Task FileInSpecifiedFolderIsCreated(
+        string folder,
+        string filename,
+        int rowsCount,
+        string[][] columns)
+    {
+        var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+        {
+            [folder] = new MockDirectoryData(),
+        });
+
+        var generator = new CsvGeneratorToFile(fileSystem);
+
+        await generator.Generate(
+            folder,
+            filename,
+            rowsCount,
+            columns);
+
+        fileSystem
+            .File
+            .Exists(Path.Combine(folder, filename))
+            .Should()
+            .BeTrue();
+    }
+
+    [Theory, AutoData]
+    public async Task Generate_NonExistingFolder_IsCreated(
+        string folder,
+        string filename,
+        int rowsCount,
+        string[][] columns)
+    {
+        var fileSystem = new MockFileSystem();
+
+        var generator = new CsvGeneratorToFile(fileSystem);
+
+        await generator.Generate(
+            folder,
+            filename,
+            rowsCount,
+            columns);
+
+        fileSystem
+            .File
+            .Exists(Path.Combine(folder, filename))
+            .Should()
+            .BeTrue();
+    }
+
+    [Theory, AutoData]
+    public async Task Generate_ExistingDestinationFilename_DontThrow(
+        string folder,
+        string filename,
+        int rowsCount,
+        string[][] columns)
+    {
+        var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+        {
+            [Path.Combine(folder, filename)] = new(string.Empty),
+        });
+
+        var generator = new CsvGeneratorToFile(fileSystem);
+
+        await generator.Generate(
+            folder,
+            filename,
+            rowsCount,
+            columns);
+
+        fileSystem
+            .File
+            .Exists(Path.Combine(folder, filename))
+            .Should()
+            .BeTrue();
+    }
+}
---
+++ GeneratorLibrary/CsvGeneratorToFile.cs
@@ -0,0 +1,20 @@
+﻿using System.IO.Abstractions;
+
+namespace GeneratorLibrary;
+
+public class CsvGeneratorToFile(
+    IFileSystem fileSystem)
+{
+    public async Task Generate(
+        string destinationFolder, 
+        string filename, 
+        int rowsCount, 
+        string[][] columns)
+    {
+        fileSystem.Directory.CreateDirectory(destinationFolder);
+
+        await fileSystem.File.WriteAllTextAsync(
+            Path.Combine(destinationFolder, filename), 
+            string.Empty);
+    }
+}

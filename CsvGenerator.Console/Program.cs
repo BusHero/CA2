155df887233716f3034d6d6b1f719bda2c0fc546
Petru Cervac <Petru.Cervac@parmenion.co.uk>
2024-10-16 16:13:58 +0100

    feat: refactpr CsvGenerator.Console

--- CsvGenerator.Console/Program.cs
+++ CsvGenerator.Console/Program.cs
@@ -1,7 +1,40 @@
-using System.CommandLine;
+using System.IO.Abstractions;
 
-using CsvGenerator.Console;
+using Cocona;
 
-var rootCommand = Root.GetRootCommand();
+using CsvGenerator;
 
-return await rootCommand.InvokeAsync(args);
+using Microsoft.Extensions.DependencyInjection;
+
+public sealed class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = CoconaApp.CreateBuilder(args);
+
+        builder.Services.AddTransient<CsvGeneratorToFile>();
+        builder.Services.AddTransient<IRandomCsvGeneratorFactory, DefaultRandomCsvGeneratorFactory>();
+        builder.Services.AddTransient<IFileSystem, FileSystem>();
+
+        var app = builder.Build();
+
+        app.AddCommand(async (
+            CsvGeneratorToFile service,
+            IFileSystem fileSystem,
+            int rows,
+            [Option] string[] columns,
+            string filename,
+            string? destination) =>
+        {
+            var realColumns = columns.Select(x => x.Split(',').ToArray()).ToArray();
+
+            await service.GenerateAsync(
+                destination ?? fileSystem.Directory.GetCurrentDirectory(), 
+                filename, 
+                rows, 
+                realColumns);
+        });
+
+        await app.RunAsync();
+    }
+}

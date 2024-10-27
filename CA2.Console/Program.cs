17c060cc18c9700f2dd4b3f74c573ff76c02c115
bus1hero <petru.cervac@gmail.com>
2024-10-28 00:06:39 +0200

    feat: register extractors

--- CA2.Console/Program.cs
+++ CA2.Console/Program.cs
@@ -5,6 +5,7 @@ using CA2.Console;
 using Cocona;
 
 using CA2.Compression;
+using CA2.Extractors;
 
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,6 +13,8 @@ var builder = CoconaApp.CreateBuilder(args);
 
 builder.Services.AddTransient<IFileSystem, FileSystem>();
 builder.Services.AddTransient<ICompressor, Compressor>();
+builder.Services.AddTransient<IExtractor, CsvExtractor>();
+builder.Services.AddTransient<IExtractor, ActsExtractor>();
 
 var app = builder.Build();
 
